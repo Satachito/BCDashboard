e366fb6b01773dbcf11f09eddfb2602b98e11846
Satachito <satoru.ogura@me.com>
2025-01-19 20:45:25 +0900

    INIT

--- renderer.js
+++ renderer.js
@@ -1,43 +1,10 @@
-console.log( main.process )
-
-AlertB.onclick				= () => alert( SomeText.value )
-
-ConfirmB.onclick			= () => confirm( SomeText.value )
-
-MessageBoxB.onclick			= async () => console.log(
-	await main.invoke(
-		'messageBox'
-	,	{	type:		MessageBoxType.value
-		,	message:	SomeText.value
-		,	buttons:	JSON.parse( MessageBoxButtons.value )
-		}
-	)
-)
-
-ErrorBoxB.onclick			= () => main.send( 'errorBox', ErrorBoxTitle.value, SomeText.value )
-
-SendClipboardB.onclick		= () => main.send( 'clipboard', ClipboardText.value )
-
-InvokeClipboardB.onclick	= async () => ClipboardText.value = await main.invoke( 'clipboard' )
-
-let
-snapshot = TextArea.value
-
 main.onData(
-	( _, $, file ) => (
-		snapshot = $
-	,	TextArea.value = $
-	,	document.title = file
-	)
+	( _, $, file ) => {}
 )
-
-TextArea.oncontextmenu		= () => main.send( 'sampleContextMenu' )
-
-TextArea.oninput			= () => main.send( 'status', { isDirty: TextArea.value !== snapshot } )
 
 main.onMenu(
 	( _, $ ) => {
-
+/*
 console.log( 'onMenu', $ )
 
 		const
@@ -56,8 +23,8 @@ console.log( 'onMenu', $ )
 			_Save( 'saveAs' )
 			break
 		}
+*/
 	}
 )
-
 
 onbeforeunload = ev => ev.preventDefault()
